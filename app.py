36691c0fd268a4918818e19386f7fe7fce753c7a
9Akshit1 <aksheru09@gmail.com>
2024-12-07 09:48:35 -0500

    added typing and fading animations

--- app.py
+++ app.py
@@ -28,5 +28,32 @@ class StartupAnimation(QWidget):
         layout.addWidget(self.label_sub)
         self.setLayout(layout)
 
-        
-        
+        self.typing_timer = QTimer(self)
+        self.typing_timer.timeout.connect(self.type_text)
+        self.sub_label_text = "Applications for the lazy"
+        self.sub_label_index = 0
+        self.typing_timer.start(50) 
+
+        self.fade_in_animation = QPropertyAnimation(self, b"windowOpacity")
+        self.fade_in_animation.setDuration(1000)  
+        self.fade_in_animation.setStartValue(0)  
+        self.fade_in_animation.setEndValue(1)    
+        self.fade_in_animation.start()
+
+        QTimer.singleShot(4000, self.start_fade_out)
+
+    def type_text(self):
+        if self.sub_label_index < len(self.sub_label_text):
+            self.label_sub.setText(self.sub_label_text[:self.sub_label_index + 1])
+            self.sub_label_index += 1
+        else:
+            self.typing_timer.stop()  
+
+    def start_fade_out(self):
+        self.fade_out_animation = QPropertyAnimation(self, b"windowOpacity")
+        self.fade_out_animation.setDuration(1000)  
+        self.fade_out_animation.setStartValue(1)   
+        self.fade_out_animation.setEndValue(0)     
+        self.fade_out_animation.start()
+
+        QTimer.singleShot(1000, self.close)  
